5834a41157f4948948c233abf2aa1a8cf1bbff6f
lauragirdwood <azevedoemello@gmail.com>
2019-05-20 13:26:27 -0300

    Adicionado laço for com dias da semana

--- js/script.js
+++ js/script.js
@@ -1,3 +1,42 @@
+var diasDaSemana = ["Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado", "Domingo"];
+
+// console.log(diasDaSemana[0]);
+// console.log(diasDaSemana[1]);
+// console.log(diasDaSemana[2]);
+// console.log(diasDaSemana[3]);
+// console.log(diasDaSemana[4]);
+// console.log(diasDaSemana[5]);
+// console.log(diasDaSemana[6]);
+
+for (let contador = 0; contador < diasDaSemana.length; ++contador){
+    console.log(contador);
+    console.log(diasDaSemana[contador]);
+    console.log(contador);
+}
+
+
+/*var corUm = "verde";
+var corDois = "preto";
+
+if (corUm == "verde" || corDois == "amarelo") {
+    console.log("Pode ser a bandeira do Brasil");
+} else {
+    console.log("Outra bandeira");
+}
+
+console.log("Programa finalizado!");*/
+
+/*var cnh = true;
+var idade = 19;
+
+if (idade >= 18 && cnh == false) {
+    console.log("Pode tirar CNH");
+} else {
+    console.log("Complete a idade mínima ou você já possui CNH.");
+}
+
+console.log("Programa finalizado!");*/
+
 /*var diaDaSemana = "Domingo";
 
 diaDaSemana = "Quarta-Feira";
@@ -21,7 +60,7 @@ console.log("Programa finalizado!");*/
 
  (diaDaSemana == "Sexta-Feira") ? console.log("Dia de futebol") : console.log("Um dia qualquer");*/
 
- var sintomas = "tossir";
+ /*var sintomas = "tossir";
 
  switch (sintomas) {
     case "tossir":
@@ -32,4 +71,4 @@ console.log("Programa finalizado!");*/
         break;
     default:
         console.log("Virose");
- }
+ }*/
