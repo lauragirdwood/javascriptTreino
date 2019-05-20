d2e359129ad9ee4efe6a77552fb03a9897a3b9a7
lauragirdwood <azevedoemello@gmail.com>
2019-05-20 10:51:59 -0300

    Adicionada lógica de if-else

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log("Helloooo todo bien");
+var diaDaSemana = "Domingo";
+
+if (diaDaSemana == "Domingo") {
+    console.log("Dia de churrasco! =D");
+} else {
+    console.log("Hoje não é dia de churrasco! =(");
+}
