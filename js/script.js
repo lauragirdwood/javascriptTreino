52e87749e435e34ce41b7e75336de2d14643c17f
lauragirdwood <azevedoemello@gmail.com>
2019-05-20 11:03:13 -0300

    Adicionadõ mais condições no if-else

--- js/script.js
+++ js/script.js
@@ -2,10 +2,16 @@ var diaDaSemana = "Domingo";
 
 diaDaSemana = "Quarta-Feira";
 
+diaDaSemana = "Sexta-Feira";
+
 if (diaDaSemana == "Domingo") {
     console.log("Dia de churrasco! =D");
 } else if   (diaDaSemana == "Quarta-Feira") {
     console.log("Dia de feijoada! =D");
+} else if   (diaDaSemana == "Sexta-Feira") {
+    console.log("Dia de maldade! =D");
 } else {
     console.log("Hoje não é dia de churrasco! =(");
 }
+
+console.log("Programa finalizado!");
