da403307da853fbc070c279e1d5465cef07cb050
lauragirdwood <azevedoemello@gmail.com>
2019-05-20 10:59:34 -0300

    Adicionado mais condições no if-else

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 var diaDaSemana = "Domingo";
 
+diaDaSemana = "Quarta-Feira";
+
 if (diaDaSemana == "Domingo") {
     console.log("Dia de churrasco! =D");
+} else if   (diaDaSemana == "Quarta-Feira") {
+    console.log("Dia de feijoada! =D");
 } else {
     console.log("Hoje não é dia de churrasco! =(");
 }
